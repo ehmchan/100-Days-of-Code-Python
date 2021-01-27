5a5dd3aeed575d215918d94b3f98577913a598b6
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-01-26 20:56:46 -0700

    created question bank
    added list with quiz data using Question class

---
+++ main.py
@@ -0,0 +1,12 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for data in question_data:
+    question_text = data['text']
+    question_answer = data['answer']
+    new_question = Question(text=question_text, answer=question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
+

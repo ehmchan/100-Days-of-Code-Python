ffcf0e94a7ce9e0fd2bf1188d40ab2866f989e9a
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-01-31 16:18:01 -0700

    create snake class
    moved previous snake related code to it's own class (OOP)

---
+++ snake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        starting_x = 0
+        for num in range(3):
+            new_segment = Turtle(shape="square")
+            new_segment.penup()
+            new_segment.color("white")
+            new_segment.goto(starting_x, 0)
+            self.segments.append(new_segment)
+            starting_x -= 20
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):  # start=2, stop=0, step=-1
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)

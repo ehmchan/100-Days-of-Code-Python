f9d1513f97020256ce72e15ca01cc535cfe9a3d4
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-01 19:51:50 -0700

    setup screen
    create screen, change colour

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Screen
+
+# create the screen
+    # in main show
+        # ping pong table
+
+screen = Screen()
+
+screen.title("Pong")
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+
+
+screen.exitonclick()
+
+# create and move a paddle
+    # separate class to create paddles
+        # paddle 1
+        # paddle 2
+# create another paddle
+# create the ball and make it move
+    # separate class for ping pong
+        # ping pong
+# detect collision with wall and bounce
+# detect collision with paddle
+# detect when paddle misses
+# keep score
+    # separate class for scoreboard
+        # scoreboard for each side
+
+

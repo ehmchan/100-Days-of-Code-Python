1550243e31e053c539bc5beed83b5db5f27b306b
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-01 20:32:02 -0700

    create paddle class
    created initial paddle class using class inheritance

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
+import time
 
 # create the screen
     # in main show
         # ping pong table
 
 screen = Screen()
-
-screen.title("Pong")
 screen.setup(width=800, height=600)
+screen.title("Pong")
 screen.bgcolor("black")
+screen.tracer(0)
 
-
-screen.exitonclick()
-
+paddle = Paddle()
 # create and move a paddle
     # separate class to create paddles
         # paddle 1
         # paddle 2
+
+screen.listen()
+screen.onkey(paddle.up, "Up")
+screen.onkey(paddle.down, "Down")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+
+
 # create another paddle
 # create the ball and make it move
     # separate class for ping pong
@@ -28,4 +40,7 @@ screen.exitonclick()
     # separate class for scoreboard
         # scoreboard for each side
 
+
+
+screen.exitonclick()
 

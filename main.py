4b3664feceebf34a8ea6a8150c0fbf575a086ea6
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-01 22:37:44 -0700

    score keeping and ball speed
    when player misses ball, increase other player's score, when players hit ball speed of ball increases for greater challenge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 # create the screen
@@ -19,6 +20,7 @@ screen.tracer(0)
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -28,7 +30,7 @@ screen.onkey(l_paddle.down, "s")
 
 game_on = True
 while game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
 
     # create the ball and make it move
@@ -43,19 +45,18 @@ while game_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    # detect when r paddle misses
-    if ball.xcor() > 380 :
-        ball.reset_position()
-
-    # detect when l paddle misses
-    if ball.xcor() < -380 :
-        ball.reset_position()
-
     # keep score
     # separate class for scoreboard
         # scoreboard for each side
-    
+    # detect when r paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
 
+    # detect when l paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
 

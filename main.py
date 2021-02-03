96e7b7c1e84d7f5ae0683d3b893acd7cf3164fa2
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-02 20:42:34 -0700

    create player
    separate player class created using class inheritance, player moves up when up arrow pressed

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+player = Player()
+
+screen.title("Turtle Crossing Game")
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+screen.onkey(player.up, "Up")
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -1,7 +1,18 @@
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+from turtle import Turtle
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.forward(MOVE_DISTANCE)
+
+

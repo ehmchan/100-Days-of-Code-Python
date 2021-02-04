09fb9533a3f55b301597118726c587150052cdd6
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-03 21:52:43 -0700

    update name replacement

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 #TODO: Create a letter using starting_letter.txt
 
+PLACEHOLDER = "[name]"
+
 names = open("./Input/Names/invited_names.txt", mode="r").readlines()
-print(names)
 
 # for each name in invited_names.txt
-new_names = []
+stripped_names = []
 for name in names:
-    new_name = name.strip("\n")
-    new_names.append(new_name)
+    stripped_name = name.strip("\n")
+    stripped_names.append(stripped_name)
 
 # Replace the [name] placeholder with the actual name.
-for name in new_names:
-    with open("./Input/Letters/starting_letter.txt", mode="r") as infile:
-        infile_data = infile.read()
-    infile_data = infile_data.replace("[name]", name)
-
-    # Save the letters in the folder "ReadyToSend".
-    with open(f"./Output/ReadyToSend/letter_to_{name}.txt", mode="w") as outfile:
-        outfile.write(infile_data)
+with open("./Input/Letters/starting_letter.txt", mode="r") as infile:
+    infile_data = infile.read()
+# Save the letters in the folder "ReadyToSend".
+for stripped_name in stripped_names:
+    outfile_data = infile_data.replace(PLACEHOLDER, stripped_name)
+    with open(f"./Output/ReadyToSend/letter_to_{stripped_name}.txt", mode="w") as outfile:
+        outfile.write(outfile_data)
 
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp

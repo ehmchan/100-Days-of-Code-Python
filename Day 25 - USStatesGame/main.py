9c1eb18a4e7868fad07315028f6961a79e4b82f1
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-06 13:34:12 -0700

    check guess and print onto image
    check if guess is among 50 states, if so print state name onto map

--- Day 25 - USStatesGame/main.py
+++ Day 25 - USStatesGame/main.py
@@ -1,10 +1,16 @@
 import turtle
+import pandas
 
+data = pandas.read_csv("50_states.csv")
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+state_name = turtle.Turtle()
+state_name.penup()
+state_name.hideturtle()
+state_name.speed("fastest")
 
 # # get coordinates of states in image (ALREADY DONE, SAVED IN 50 STATES CSV)
 # def get_mouse_click_coor(x, y):
@@ -13,14 +19,19 @@ turtle.shape(image)
 # turtle.mainloop()
 
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
 
 # convert guess to title case
 guess = answer_state.title()
 
 # check if guess is among 50 states
-
 # write correct guesses onto map
+states = data["state"].to_list()
+if guess in states:
+    state = data[data.state == guess]
+    x_coord = int(state.x)
+    y_coord = int(state.y)
+    state_name.goto(x_coord, y_coord)
+    state_name.write(guess, align="center", font=("Arial", 8, "normal"))
 
 # use loop to allow user to keep guessing
 
@@ -29,3 +40,4 @@ guess = answer_state.title()
 # keep track of score
 
 
+screen.exitonclick()

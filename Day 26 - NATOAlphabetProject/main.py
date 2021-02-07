57f6be1717865f277794793cd30c3800cac0039e
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-06 22:00:38 -0700

    create dictionary and list of phonetic codes
    create dictionary from csv file using pandas, convert word to phonetic codes from dictionayr and store in list

--- Day 26 - NATOAlphabetProject/main.py
+++ Day 26 - NATOAlphabetProject/main.py
@@ -1,17 +1,12 @@
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+#TODO 1. Create a dictionary in this format {"A": "Alfa", "B": "Bravo"}:
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+alphabet = {row.letter:row.code for (index, row) in data.iterrows()}
+# print(alphabet)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+phonetic_code = [alphabet[letter] for letter in word]
+print(phonetic_code)
 

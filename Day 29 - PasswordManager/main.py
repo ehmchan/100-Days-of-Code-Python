8f3c809f589b08fdf853ee20d4ac8c71091f9a89
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-10 20:27:52 -0700

    save pw to file
    created function to save pw entered to file when add button is clicked

--- Day 29 - PasswordManager/main.py
+++ Day 29 - PasswordManager/main.py
@@ -2,6 +2,15 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_box.get()
+    email = email_box.get()
+    password = pw_box.get()
+    with open("data.txt", mode="a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+        website_box.delete(0, 'end')
+        pw_box.delete(0, 'end')
+        website_box.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -18,12 +27,14 @@ website_label.grid(column=0, row=1)
 
 website_box = Entry(width=51)
 website_box.grid(column=1, row=1, columnspan=2)
+website_box.focus()
 
 email_label = Label(text="Email/Username:", font=("Arial", 10, "normal"))
 email_label.grid(column=0, row=2)
 
 email_box = Entry(width=51)
 email_box.grid(column=1, row=2, columnspan=2)
+email_box.insert(0, "eugenia@email.com")
 
 pw_label = Label(text="Password:", font=("Arial", 10, "normal"))
 pw_label.grid(column=0, row=3)
@@ -34,9 +45,7 @@ pw_box.grid(column=1, row=3)
 generate_pw = Button(text="Generate Password")
 generate_pw.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=43)
+add_button = Button(text="Add", width=43, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
-
-
 
 window.mainloop()

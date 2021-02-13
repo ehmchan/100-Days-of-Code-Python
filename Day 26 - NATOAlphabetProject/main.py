56f9b1ccc56cf66c3036d4c419887064a4deea8a
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-12 22:11:20 -0700

    day 30 exercise
    update with error handling learned in day 30

--- Day 26 - NATOAlphabetProject/main.py
+++ Day 26 - NATOAlphabetProject/main.py
@@ -6,7 +6,26 @@ alphabet = {row.letter:row.code for (index, row) in data.iterrows()}
 # print(alphabet)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-phonetic_code = [alphabet[letter] for letter in word]
-print(phonetic_code)
+# converting = True
+# while converting:
+#     word = input("Enter a word: ").upper()
+#     try:
+#         phonetic_code = [alphabet[letter] for letter in word]
+#     except KeyError:
+#         print("Sorry, only letters in the alphabet please.")
+#         converting = True
+#     else:
+#         print(phonetic_code)
+#         converting = False
 
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_code = [alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_code)
+
+generate_phonetic()

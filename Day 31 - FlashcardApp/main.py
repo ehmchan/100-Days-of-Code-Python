764db2124a55e672de3d669db5feed980a9f6f47
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-13 17:48:27 -0700

    add ability to flip card
    card flips to english translation after 3s

--- Day 31 - FlashcardApp/main.py
+++ Day 31 - FlashcardApp/main.py
@@ -7,23 +7,37 @@ BACKGROUND_COLOR = "#B1DDC6"
 data = pandas.read_csv("./data/french_words.csv")
 words = data.to_dict(orient="records")
 
+new_word = {}
+
 # create new flash cards
 def create_cards():
+    global new_word, flip_timer
+    window.after_cancel(flip_timer)
     new_word = random.choice(words)
-    canvas.itemconfig(language, text="French")
-    canvas.itemconfig(word, text=new_word['French'])
+    canvas.itemconfig(language, text="French", fill="black")
+    canvas.itemconfig(word, text=new_word['French'], fill="black")
+    canvas.itemconfig(canvas_image, image=front_card)
+    flip_timer = window.after(3000, func=flip_card)
 
+# flip cards
+def flip_card():
+    canvas.itemconfig(canvas_image, image=back_card)
+    canvas.itemconfig(language, text="English", fill="white")
+    canvas.itemconfig(word, text=new_word['English'], fill="white")
 
 # UI Setup
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=front_card)
-language = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+back_card = PhotoImage(file="./images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=front_card)
+language = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"), fill="black")
+word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"), fill="black")
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_pic = PhotoImage(file="./images/wrong.png")

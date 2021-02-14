2425d35be8978effba5d04d6b34b14a59d3737aa
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-13 17:14:31 -0700

    create new cards
    randomly choose french word from csv file using pandas and show onto card

--- Day 31 - FlashcardApp/main.py
+++ Day 31 - FlashcardApp/main.py
@@ -1,6 +1,18 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("./data/french_words.csv")
+words = data.to_dict(orient="records")
+
+# create new flash cards
+def create_cards():
+    new_word = random.choice(words)
+    canvas.itemconfig(language, text="French")
+    canvas.itemconfig(word, text=new_word['French'])
+
 
 # UI Setup
 window = Tk()
@@ -10,16 +22,18 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=front_card)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+language = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_pic = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_pic, highlightthickness=0)
+wrong_button = Button(image=wrong_pic, highlightthickness=0, command=create_cards)
 wrong_button.grid(column=0, row=1)
 
 right_pic = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_pic, highlightthickness=0)
+right_button = Button(image=right_pic, highlightthickness=0, command=create_cards)
 right_button.grid(column=1, row=1)
+
+create_cards()
 
 window.mainloop()

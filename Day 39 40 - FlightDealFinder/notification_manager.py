489048568de79b5719d245f1e47ee0840820c712
ehmchan <71477935+ehmchan@users.noreply.github.com>
2021-02-23 22:39:52 -0700

    send email alert and link to google flight
    using sheety API obtain list of emails entered in customer acquisition step, send alert to each email for each cheap flight with link to google flights

--- Day 39 40 - FlightDealFinder/notification_manager.py
+++ Day 39 40 - FlightDealFinder/notification_manager.py
@@ -1,6 +1,7 @@
 #This class is responsible for sending notifications with the deal flight details.
 from twilio.rest import Client
 import os
+import smtplib
 
 
 class NotificationManager:
@@ -9,6 +10,9 @@ class NotificationManager:
         self.twilio_auth_token = os.environ["twilio_auth_token"]
         self.twilio_phone_num = os.environ["twilio_phone_num"]
         self.my_phone_num = os.environ["my_phone_num"]
+        self.my_email = os.environ["my_email"]
+        self.email_pw = os.environ["email_pw"]
+
 
     def send_alert(self, price, city, code, out, ret):
         self.client = Client(self.twilio_account_sid, self.twilio_auth_token)
@@ -22,3 +26,14 @@ class NotificationManager:
         )
 
         print(self.message.status)
+
+    def send_email(self, price, city, code, out, ret, to_email):
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=self.my_email, password=self.email_pw)
+            connection.sendmail(
+                from_addr=self.my_email,
+                to_addrs=to_email,
+                msg=f"Subject:Flight Deal!\n\nLow price alert! Only ${price} to fly from LONDON-STN to {city}-{code}, from {out} to {ret}.\n"
+                    f"https://www.google.com/flights?hl=en#flt=STN.{code}.{out}*{code}.STN.{ret}")
+
